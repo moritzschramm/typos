239f449427b7a70004eae6032ea26062a120ae99
Moritz Schramm <moritz.schramm1@gmail.com>
2017-08-10 23:23:59 +0200

    moved to app/Models

---
+++ app/Models/User.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+  use Notifiable;
+
+  protected $table = 'users';
+  protected $primaryKey = 'id_user';
+
+  /**
+   * The attributes that should be hidden for arrays.
+   *
+   * @var array
+   */
+  protected $hidden = [
+      'password', 'remember_token', 'token',
+  ];
+}
