0a0e0236004ee3afdf37c05d15e369e5fe34188f
Moritz Schramm <moritz.schramm1@gmail.com>
2017-08-11 16:12:14 +0200

    added verification mail builder

---
+++ app/Mail/VerifyAccountMail.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class VerifyAccountMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $verifyUrl;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($verifyUrl)
+    {
+      $this->verifyUrl = $verifyUrl;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+      return $this->from(config('mail.from.noreply'))->markdown('emails.verification');
+    }
+}
