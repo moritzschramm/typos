643f38d4a2045eeba81974277b0b55059996e5c8
Moritz Schramm <moritz.schramm1@gmail.com>
2017-08-11 16:10:17 +0200

    adding no-reply email

--- config/mail.php
+++ config/mail.php
@@ -58,6 +58,8 @@ return [
     'from' => [
         'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
         'name' => env('MAIL_FROM_NAME', 'Example'),
+
+        'noreply' => 'no-reply@typos.space',
     ],
 
     /*
