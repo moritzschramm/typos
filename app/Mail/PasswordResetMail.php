2f0ed0f7fc0e035825f4575254edd04719a40751
Moritz Schramm <moritz.schramm1@gmail.com>
2017-08-15 19:06:55 +0200

    added PasswordResetMail

---
+++ app/Mail/PasswordResetMail.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class PasswordResetMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $resetUrl;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($resetUrl)
+    {
+      $this->resetUrl = $resetUrl;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->from(config('mail.from.noreply'))->markdown('emails.password_reset');
+    }
+}
