40c715beaf44b0e5a86c048561f468202e1c47c5
moritzschramm <moritz.schramm1@gmail.com>
2017-09-17 01:55:40 +0200

    renamed to CreateLectionNonce

---
+++ app/Traits/CreateLectionNonce.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Traits;
+
+use App\Models\LectionNonce;
+
+trait CreateLectionNonce {
+
+  /**
+    * Creates a lection nonce without generating a nonce and timestamp
+    *
+    * @param int $userId
+    * @param int $characterAmount: the total amount of characters the lections will have
+    * @param boolean $isLection (default false): used for xp calculation (lections always 10XP)
+    * @return string $nonce
+    */
+  public function createLectionNonce($userId, $characterAmount, $isLection = false)
+  {
+    $nonce                    = new LectionNonce;
+    $nonce->id_user           = $userId;
+    $nonce->character_amount  = $characterAmount;
+    $nonce->is_lection        = $isLection;
+
+    session()->put('nonce', $nonce);
+  }
+}
