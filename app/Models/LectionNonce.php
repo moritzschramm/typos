92489879467faf4b03e65502482740425bca1f6c
moritzschramm <moritz.schramm1@gmail.com>
2017-09-17 01:56:34 +0200

    added public properties -> no database model anymore, only for session

--- app/Models/LectionNonce.php
+++ app/Models/LectionNonce.php
@@ -2,17 +2,27 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+/**
+  * NOTE: this model is only used in the session (since it's only in use for
+  * a short period of time and does store only temporary data)
+  * A LectionNonce gets created when the user starts a lection/training.
+  * This nonce should be passed to the javascript app
+  * which sends it back when the results get uploaded (after that the nonce
+  * can be deleted).
+  * The nonce as well as timestamps, the user's velocity
+  * and the amount of characters will be used to determine
+  * if a bot is trying to upload fake results.
+  *
+  * Also note that the nonce will be created first without an actual nonce and
+  * timestamp, but to store the character amount. When the user makes his first
+  * input, the nonce and timestamp should be assigned.
+  */
 
-class LectionNonce extends Model
+class LectionNonce
 {
-  protected $table = 'lection_nonces';
-  protected $primaryKey = 'id_lection_nonce';
-
-  protected $fillable = [ 'id_user', 'nonce', 'character_amount', 'is_lection' ]; 
-
-  public function user()
-  {
-    return $this->belongsTo(User::class, 'id_user', 'id_user');
-  }
+  public $id_user;
+  public $nonce;
+  public $character_amount;
+  public $timestamp;
+  public $is_lection;
 }
