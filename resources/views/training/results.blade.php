2eb08dad5d36ddf9d6fdbc57f1b08fd50530e3f2
moritzschramm <moritz.schramm1@gmail.com>
2017-09-18 18:22:30 +0200

    added xp chart and cheated message

--- resources/views/training/results.blade.php
+++ resources/views/training/results.blade.php
@@ -16,7 +16,7 @@
 
     <h1>@lang('training.results.title')</h1>
 
-    <div style="margin: 20px auto; width: 20%" data-toggle="tooltip" data-placement="right" title="heutige XP">
+    <div style="margin: 20px auto; width: 20%" data-toggle="tooltip" data-placement="right" title="@lang('training.results.todaysXP')">
       <canvas id="graph" width="800" height="800"></canvas>
     </div>
 
@@ -24,10 +24,26 @@
     <div class="info">
       <h2 id="heading_goal"></h2>
 
+      @if($cheated)
+        <h2>@lang('training.results.cheated')</h2>
+      @endif
+
       <h3>XP: {{ $xp }}</h3>
-      <p>@lang('training.results.amountCorrects'): {{ $keystrokes - $errors }}</p>
-      <p>@lang('training.results.amountErrors'): {{ $errors }}</p>
-      <p>@lang('training.results.avgSpeed'): {{ $velocity }} @lang('training.velocityUnit')</p>
+
+      <div class="row">
+        <div class="col-xs-6 text-right">
+          <p>@lang('training.results.amountCorrects'):</p>
+          <p>@lang('training.results.amountErrors'):</p>
+          <p>@lang('training.results.avgSpeed'):</p>
+        </div>
+        <div class="col-xs-6 text-left">
+          <p>{{ $keystrokes - $errors }}</p>
+          <p>{{ $errors }}</p>
+          <p>{{ $velocity }} @lang('training.velocityUnit')</p>
+        </div>
+      </div>
+
+
     </div>
 
     <div class="text-right">
@@ -40,14 +56,56 @@
 @endsection
 
 @section('footer')
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.0/Chart.bundle.min.js"></script>
 <script>
 var xp = {{ $xp }};
 var goal = 30;
 
 $(document).ready(function() {
+
+  // bootstrap tooltips
   $('[data-toggle="tooltip"]').tooltip();
-});
+
+  // chart
+  var ctx = document.getElementById("graph");
+  var dif = Math.max(0, goal - xp);
+
+  Chart.defaults.global.legend.display = false;
+  Chart.defaults.global.defaultFontFamily = "'Montserrat', 'Arial', 'sans-serif'";
+
+  var data = {
+      labels: [
+          "@lang('training.results.currentXP')",
+          "@lang('training.results.missingXP')"
+      ],
+      datasets: [
+          {
+              data: [xp, dif],
+              backgroundColor: [
+                  "#ff5722",
+                  "#ffab91"
+              ],
+              borderColor: [
+                  "#ff5722",
+                  "#ffab91"
+              ],
+              hoverBackgroundColor: [
+                  "#ff5722",
+                  "#ffab91"
+              ]
+          }]
+  };
+
+  var chart = new Chart(ctx, {
+      type: 'doughnut',
+      data: data,
+      animation:{
+            animateScale: true
+      },
+      options: {
+        cutoutPercentage: 70
+      }
+    });
+  });
 </script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.1.6/Chart.bundle.min.js"></script>
-<script src="/scripts/complete_chart.js"></script>
 @endsection
