d559416f0b8f0502b3a88e2ac1b63e9271242289
moritzschramm <moritz.schramm1@gmail.com>
2017-09-22 00:24:45 +0200

    implementing Exercise- and LectionController

--- app/Http/Controllers/Training/ExerciseController.php
+++ app/Http/Controllers/Training/ExerciseController.php
@@ -5,7 +5,64 @@ namespace App\Http\Controllers\Training;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use Auth;
+use App\Models\Exercise;
+use App\Traits\CreateLectionNonce;
+
 class ExerciseController extends Controller
 {
-    //
+  use CreateLectionNonce;
+
+  /**
+    * Middlewares:
+    *  - auth
+    *
+    */
+  public function __construct()
+  {
+    $this->middleware('auth');
+  }
+
+  /**
+    * shows training view
+    *
+    * @param integer $exerciseId (external_id)
+    * @return view
+    */
+  public function showLection($exerciseId)
+  {
+    return view('training.app', ['dataURI' => "/exercise/$exerciseId", 'keyboardLayout' => 'de-de']);
+  }
+
+  /**
+    * returns lection as JSON
+    *
+    * @param integer $exerciseId (external_id)
+    * @return JSON | abort(404)
+    */
+  public function getLection($exerciseId)
+  {
+    $exercise = Exercise::where('id_exercise', $exerciseId)->first();
+
+    if($exercise) {
+
+      if(Auth::user()->id_user !== $exercise->id_user && is_null($exercise->is_public)) {
+        // exercise does not belong to user and is not publicly available
+        abort(403);
+      }
+
+      $this->createLectionNonce($exercise->characterAmount);
+
+      return [
+        'meta' => [
+          'mode' => 'block',     // to prepare content of exercise properly
+        ],
+        'lines' => $exercise->content,
+      ];
+
+    } else {      // exercise does not exists, throw 404
+
+      abort(404);
+    }
+  }
 }
--- app/Http/Controllers/Training/LectionController.php
+++ app/Http/Controllers/Training/LectionController.php
@@ -5,7 +5,64 @@ namespace App\Http\Controllers\Training;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use Auth;
+use App\Models\Lection;
+use App\Traits\CreateLectionNonce;
+
 class LectionController extends Controller
 {
-    //
+  use CreateLectionNonce;
+
+  /**
+    * Middlewares:
+    *  - auth
+    *
+    */
+  public function __construct()
+  {
+    $this->middleware('auth');
+  }
+
+  /**
+    * shows training view
+    *
+    * @param integer $lectionId (external_id)
+    * @return view
+    */
+  public function showLection($lectionId)
+  {
+    return view('training.app', ['dataURI' => "/lection/$lectionId", 'keyboardLayout' => 'de-de']);
+  }
+
+  /**
+    * returns lection as JSON
+    *
+    * @param integer $lectionId (external_id)
+    * @return JSON | abort(404)
+    */
+  public function getLection($lectionId)
+  {
+    $locale = Auth::user()->locale;
+
+    $lection = Lection::where([
+      ['external_id', '=', $lectionId],
+      ['locale',      '=', $locale],
+    ])->first();
+
+    if($lection) {
+
+      $this->createLectionNonce($lection->characterAmount, $isLection=true);
+
+      return [
+        'meta' => [
+          'mode' => 'prepared',     // content of lection is already prepared (lines < 20 chars)
+        ],
+        'lines' => explode("\n", $lection->content),
+      ];
+
+    } else {      // if lection does not exists, throw 404
+
+      abort(404);
+    }
+  }
 }
