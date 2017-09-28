3e6c16ce80b86d7771eda5900d03a0d1e7d9f0a4
moritzschramm <moritz.schramm1@gmail.com>
2017-09-28 22:40:40 +0200

    implemeted index() function

--- app/Http/Controllers/Dashboard/DashboardController.php
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers\Dashboard;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use Auth;
+use App\Models\Lection;
+use App\Models\Exercise;
+
 class DashboardController extends Controller
 {
   /**
@@ -16,8 +20,21 @@ class DashboardController extends Controller
     $this->middleware('auth');
   }
 
+  /**
+    * show dashboard index view
+    *
+    * @return view
+    */
   public function showDashboard()
   {
-    return view('dashboard.index');
+    $locale = Auth::user()->preferences->getKeyboardLocale();
+
+    $lections   = Lection::where('locale', $locale)->get();
+    $exercises  = Exercise::where('id_user', Auth::user()->id_user)->get();
+
+    return view('dashboard.index', [
+      'lections'  => $lections,
+      'exercises' => $exercises,
+    ]);
   }
 }
