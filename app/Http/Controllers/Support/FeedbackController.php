047ac425e70c3eb38b7bc2e2a23bf53fb14875cd
moritzschramm <moritz.schramm1@gmail.com>
2017-10-02 22:00:46 +0200

    implemented support and feedback controller

--- app/Http/Controllers/Support/FeedbackController.php
+++ app/Http/Controllers/Support/FeedbackController.php
@@ -5,7 +5,57 @@ namespace App\Http\Controllers\Support;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use Validator, Auth;
+
+use App\Mail\SupportMail;
+
 class FeedbackController extends Controller
 {
-    //
+  /**
+    * Middlewares:
+    *  - throttle (max 6 request, 10 min cooldown)
+    *
+    */
+  public function __construct()
+  {
+    $this->middleware('throttle:6,10')->only('sendFeedback');
+  }
+
+  /**
+    * validates request from feedback form and sends email to support team
+    *
+    * @param Request
+    * @return JSON
+    */
+  public function sendFeedback(Request $request)
+  {
+    $validator = Validator::make($request->all(), [
+      'email'   => 'required|email',
+      'message' => 'required',
+    ], [
+      'required'  => 'errors.required',
+      'email'     => 'errors.email',
+    ]);
+
+    if($validator->fails()) {
+
+      return [
+        'status' => 'error',
+        'validator' => $validator->messages(),
+      ];
+
+    } else {
+
+      $email = $request->input('email');
+      $message = $request->input('message');
+      $userId = Auth::check() ? Auth::user()->id_user : -1;
+
+      Mail::to(config('mail.from.support'))
+            ->send(new SupportMail('Feedback', $email, $message, $userId));
+
+      return [
+        'status' => 'success',
+      ];
+    }
+  }
 }
--- app/Http/Controllers/Support/SupportController.php
+++ app/Http/Controllers/Support/SupportController.php
@@ -5,7 +5,62 @@ namespace App\Http\Controllers\Support;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use Validator, Auth, Mail;
+use App\Mail\SupportMail;
+
 class SupportController extends Controller
 {
-    //
+  /**
+    * Middlewares:
+    *  - throttle (max 6 request, 10 min cooldown)
+    *
+    */
+  public function __construct()
+  {
+    $this->middleware('throttle:6,10')->only('sendSupportRequest');
+  }
+
+  /**
+    * show support form
+    *
+    * @return view
+    */
+  public function showSupport()
+  {
+    return view('public.support');
+  }
+
+  /**
+    * validate input from support form and send mail to support team
+    *
+    * @param Request
+    * @return
+    */
+  public function sendSupportRequest(Request $request)
+  {
+    $validator = Validator::make($request->all(), [
+      'email'   => 'required|email',
+      'message' => 'required',
+    ], [
+      'required'  => 'errors.required',
+      'email'     => 'errors.email',
+    ]);
+
+    if($validator->fails()) {
+
+      return back()->withInput()->withErrors($validator);
+
+    } else {
+
+
+      $email = $request->input('email');
+      $message = $request->input('message');
+      $userId = Auth::check() ? Auth::user()->id_user : -1;
+
+      Mail::to(config('mail.from.support'))
+            ->send(new SupportMail('Support request', $email, $message, $userId));
+
+      return redirect('/support')->with('notification-success', 'mail.support.sent');
+    }
+  }
 }
