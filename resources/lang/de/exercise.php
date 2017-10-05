5b555eeeda39bdd4b285a92070e1a4e6756dd7fc
Moritz Schramm <moritz.schramm1@gmail.com>
2017-10-05 13:11:59 +0200

    added translations for exercise view

---
+++ resources/lang/de/exercise.php
@@ -0,0 +1,21 @@
+<?php
+
+/***
+  ** DE - DEUTSCH - GERMAN
+  **/
+
+return [
+
+  'title'   => 'Titel der Übung',
+  'content' => 'Inhalt der Übung',
+
+  'create' => [
+    'title' => 'Neue Übung erstellen',
+  ],
+  'created' => 'Übung erstellt.',
+
+  'edit' => [
+    'title' => 'Übung bearbeiten',
+  ],
+  'edited' => 'Änderungen gespeichert.',
+];
---
+++ resources/lang/en/exercise.php
@@ -0,0 +1,22 @@
+<?php
+
+/***
+  ** EN - ENGLISH
+  **/
+
+return [
+
+  'title'   => 'Title of exercise',
+  'content' => 'Content of exercise',
+
+  'create' => [
+    'title' => 'Create exercise',
+  ],
+  'created' => 'Exercise created.',
+
+  'edit' => [
+    'title' => 'Edit Exercise',
+  ],
+  'edited' => 'Exercise saved.',
+
+];
