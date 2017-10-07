8e873d847260bbc4fd265bf9398f399c2cbc9a11
Moritz Schramm <moritz.schramm1@gmail.com>
2017-10-07 21:27:58 +0200

    assigning keyboardLayout (either from user preferences or from given locale)

--- app/Traits/CreateAppView.php
+++ app/Traits/CreateAppView.php
@@ -8,16 +8,41 @@ trait CreateAppView {
     * creats training app view
     *
     * @param string $dataURI: will be passed to app to retrieve sequence via XHR POST
+    * @param string|User $userOrLocale: a user object (if user is logged in) or locale string (e.g. for trial)
     * @param array $data (optional): the data that will be passed to the view
     *        NOTE: $data will be merged with a preset array containing defaults
     * @return view
     */
-  public function createAppView($dataURI, $data = array()) {
+  public function createAppView($dataURI, $userOrLocale, $data = array()) {
+
+    if($userOrLocale instanceof \App\Models\User) {   # argument is instance of User class
+
+      $preferences = $userOrLocale->preferences;
+
+      $data['keyboardLayout'] = $preferences->keyboard;
+      $data['setting'] = [
+        'assignment'  => $preferences->show_assignment,
+        'divider'     => $preferences->show_divider,
+        'keyboard'    => $preferences->show_keyboard,
+      ];
+
+    } else {  # argument is a locale string
+
+      switch($userOrLocale) {
+
+        case 'en':  $layout = 'en-us'; break;
+        case 'de':  $layout = 'de-de'; break;
+        default:    $layout = 'en-us'; break;
+      }
+
+      $data['keyboardLayout'] = $layout;
+    }
+
 
     # add additional data to $data array, $data parameter will overwrite any defaults from hardcoded array
     $data = array_merge([
       'dataURI'         => $dataURI,
-      'keyboardLayout'  => 'de-de',
+      'keyboardLayout'  => 'en-us',
       'setting' => [
         'assignment'  => 'false',
         'divider'     => 'false',
