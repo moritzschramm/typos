f0a7d39ea9b0bbaaffde07800ea27d58278dfb47
Moritz Schramm <moritz.schramm1@gmail.com>
2017-10-12 22:00:52 +0200

    creating 2 additional test users (one unverified, one without email)

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -13,7 +13,24 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
+      # create "normal" test user
       $user = factory(User::class)->states('testuser', 'verified')->make();
+      $user->save();
+
+      $preference = factory(UserPreference::class)->make();
+      $preference->id_user = $user->id_user;
+      $preference->save();
+
+      # create user account without email
+      $user = factory(User::class)->states('no-email-user', 'verified')->make();
+      $user->save();
+
+      $preference = factory(UserPreference::class)->make();
+      $preference->id_user = $user->id_user;
+      $preference->save();
+
+      # create user account that hasn't been verified yet
+      $user = factory(User::class)->states('unverified-user')->make();
       $user->save();
 
       $preference = factory(UserPreference::class)->make();
