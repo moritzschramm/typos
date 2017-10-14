90a2b19e0861c9a3f226cd97e6ed16c8f5e22bc6
Moritz Schramm <moritz.schramm1@gmail.com>
2017-10-14 14:18:45 +0200

    added favicon meta tags

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -3,12 +3,12 @@
 <head>
   <title>@yield('title') - Typos</title>
   <meta charset="utf-8">
-  <link rel="icon" href="/favicon.ico" type="image/vnd.microsoft.icon">
+  @include('layouts.favicon-meta')
   <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1">
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <link href='https://fonts.googleapis.com/css?family=Arvo|Montserrat|Raleway|Lato|Open+Sans:400,600' rel='stylesheet' type='text/css'>
-  <link href="/css/bootstrap.min.css" rel="stylesheet" type="text/css">
-	<link href="/css/main.min.css" rel="stylesheet" type="text/css">
+  <link href="/res/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+	<link href="/res/css/main.min.css" rel="stylesheet" type="text/css">
 
   @yield('header')
 </head>
@@ -23,8 +23,8 @@
 
   @include('layouts.footer')
 
-  <script src="/js/jquery.min.js"></script>
-	<script src="/js/bootstrap.min.js"></script>
+  <script src="/res/js/jquery.min.js"></script>
+	<script src="/res/js/bootstrap.min.js"></script>
   <script>
   $(document).ready(function() {
     $(".notification-close").on("click", function() {
