372f484174299c25fa456b911ea95763cdc77643
Moritz Schramm <moritz.schramm1@gmail.com>
2017-10-18 21:51:02 +0200

    move functions from traits to model

--- app/Models/Exercise.php
+++ app/Models/Exercise.php
@@ -39,6 +39,27 @@ class Exercise extends Model
     return $id;
   }
 
+  /**
+    * checks if exercise contains any invalid characters
+    *
+    * @param string $content
+    * @return null|string $invalidChars
+    */
+  public static function findInvalidCharacters($content)
+  {
+    $allowed = '\.ß+#*~-_,;:!"\'?§$%&()[]{}°<>|@€µ=äöü';
+    $allowed_already_escaped = '\/ ';   # whitespace is intended
+    $always_allowed = 'a-zA-Z0-9\r\n\t';
+    $regex = '/(*UTF8)[^' . $always_allowed . $allowed_already_escaped . preg_quote($allowed) . ']/';
+    # the (*UTF8) is necessary because it's 2017 and php still sucks _abysmally_ at unicode
+    # @see https://stackoverflow.com/questions/1725227/preg-match-and-utf-8-in-php
+
+
+    $match_counter = preg_match_all($regex, $content, $matches);
+
+    return $match_counter === 0 ? null : implode($matches[0]);
+  }
+
   // database relationship
   public function user()
   {
