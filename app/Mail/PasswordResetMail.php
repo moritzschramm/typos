8841f353c95711c3da9ef53fafb353381e37b88b
Moritz Schramm <moritz.schramm1@gmail.com>
2017-10-20 20:10:47 +0200

    implement ShouldQueue for Mails

--- app/Mail/PasswordResetMail.php
+++ app/Mail/PasswordResetMail.php
@@ -7,7 +7,7 @@ use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
-class PasswordResetMail extends Mailable
+class PasswordResetMail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
--- app/Mail/SupportMail.php
+++ app/Mail/SupportMail.php
@@ -7,7 +7,7 @@ use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
-class SupportMail extends Mailable
+class SupportMail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
